fe58191f1b3452adaf98eeec1cada899c36132f4
mike <mike.hoffman1218@gmail.com>
2019-12-08 21:15:26 -0600

    removed barometer/compass drivers from spf3 target

--- src/main/target/SPRACINGF3/target.mk
+++ src/main/target/SPRACINGF3/target.mk
@@ -3,13 +3,13 @@ F3_TARGETS  += $(TARGET)
 FEATURES    = ONBOARDFLASH
 
 TARGET_SRC = \
-            drivers/accgyro/accgyro_mpu.c \
-            drivers/barometer/barometer_ms5611.c \
-            drivers/barometer/barometer_bmp085.c \
-            drivers/barometer/barometer_bmp280.c \
-            drivers/compass/compass_ak8975.c \
-            drivers/compass/compass_hmc5883l.c \
-            drivers/compass/compass_qmc5883l.c
+            drivers/accgyro/accgyro_mpu.c
+            //drivers/barometer/barometer_ms5611.c \
+            //drivers/barometer/barometer_bmp085.c \
+            //drivers/barometer/barometer_bmp280.c \
+            //drivers/compass/compass_ak8975.c \
+            //drivers/compass/compass_hmc5883l.c \
+            //drivers/compass/compass_qmc5883l.c
 
 ifeq ($(TARGET), FLIP32F3OSD)
 FEATURE_CUT_LEVEL = 2
